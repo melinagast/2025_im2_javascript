a597c08cae192d9a618a5a69ad1e6d3c43b26db9
melinagast <melina.gast@stud.fhgr.ch>
2025-04-16 13:07:26 +0200

    function

--- code-alongs/01_passwort_checker/script.js
+++ code-alongs/01_passwort_checker/script.js
@@ -1,4 +1,25 @@
 console.log('hi script.js')
-let passwort = "javascriptmachtspass";
 
-if (passwort)
+let username = prompt ("Username");
+let passwort = prompt ("Passwort eingeben");
+
+checkStringLength(username, 7);
+checkStringLength(passwort, 12);
+
+function checkPassword(pw) {
+    if (pw.length > 11) {
+        console.log ("Dein Passwort ist lang genug.")
+    } else {
+        console.log ("Bitte verwende ein längeres Passwort.")
+    }
+}
+ 
+function checkStringLength(testString, testlength) {
+    if (testString.length >= testlength) {
+        console.log ("Dein Text lang genug");
+    } else {
+        console.log ("Bitte verwende einen längeren Text");
+    }
+}
+
+
